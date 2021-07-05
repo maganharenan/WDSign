f68da481dee300a3e731c522a5d90f7a8a62dff8
Renan <maganha.renan@gmail.com>
2021-07-05 14:09:51 -0300

    last updates

--- Sources/WDSign/WDSignDocumentView.swift
+++ Sources/WDSign/WDSignDocumentView.swift
@@ -9,51 +9,57 @@ import SwiftUI
 
 public struct WDSignDocumentView: View {
     public var body: some View {
-        VStack(spacing: 0) {
-            /// Navigation  bar
-            HStack {
-                Button(action: {
-                    self.presentationMode.wrappedValue.dismiss()
-                }, label: {
-                    Text("Cancel")
-                        .font(.body)
-                        .foregroundColor(Color(#colorLiteral(red: 0.9176470588, green: 0.462745098, blue: 0.4078431373, alpha: 1)))
-                        .frame(width: 100, height: 44)
-                })
-                
-                Text(documentLayoutInfo?.title ?? "")
-                    .font(.title2)
-                    .foregroundColor(Color(#colorLiteral(red: 0.2352941176, green: 0.4117647059, blue: 0.4980392157, alpha: 1)))
-                    .frame(maxWidth: .infinity)
-                
-                Button(action: {
+        ZStack {
+            VStack(spacing: 0) {
+                /// Navigation  bar
+                HStack {
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Text("Cancel")
+                            .font(.body)
+                            .foregroundColor(Color(#colorLiteral(red: 0.9176470588, green: 0.462745098, blue: 0.4078431373, alpha: 1)))
+                            .frame(width: 100, height: 44)
+                    })
                     
-                }, label: {
-                    Text("Save")
-                        .font(.headline)
-                        .foregroundColor(Color(#colorLiteral(red: 0.4549019608, green: 0.7333333333, blue: 0.7098039216, alpha: 1)))
-                        .frame(width: 100, height: 44)
-                })
+                    Text(documentLayoutInfo?.title ?? "")
+                        .font(.title2)
+                        .foregroundColor(Color(#colorLiteral(red: 0.2352941176, green: 0.4117647059, blue: 0.4980392157, alpha: 1)))
+                        .frame(maxWidth: .infinity)
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Save")
+                            .font(.headline)
+                            .foregroundColor(Color(#colorLiteral(red: 0.4549019608, green: 0.7333333333, blue: 0.7098039216, alpha: 1)))
+                            .frame(width: 100, height: 44)
+                    })
+                }
+                .frame(maxWidth: .infinity, maxHeight: 50)
+                
+                /// Document layout
+                ZStack {
+                    if let backgroundWatermark = documentLayoutInfo?.watermark {
+                        Image(backgroundWatermark)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    
+                    VStack(spacing: 0) {
+                        Text(documentLayoutInfo.documentText)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                        
+                        SignFieldView(appDelegate: appDelegate, showModal: $showModal)
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(#colorLiteral(red: 0.9333333333, green: 0.9529411765, blue: 0.9607843137, alpha: 1)))
             }
-            .frame(maxWidth: .infinity, maxHeight: 50)
             
-            /// Document layout
-            ZStack {
-                if let backgroundWatermark = documentLayoutInfo?.watermark {
-                    Image(backgroundWatermark)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        .aspectRatio(contentMode: .fit)
-                }
-                
-                VStack(spacing: 0) {
-                    Text(documentLayoutInfo.documentText)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                
-                    SignFieldView(appDelegate: appDelegate, showModal: $showModal)
-                }
+            if showModal {
+                SignatureBoxView()
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(#colorLiteral(red: 0.9333333333, green: 0.9529411765, blue: 0.9607843137, alpha: 1)))
         }
         .navigationBarHidden(true)
     }

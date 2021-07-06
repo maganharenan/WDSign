8c489b25853fa8e1abe39470df1a5a91a7ea63e0
Renan <maganha.renan@gmail.com>
2021-07-06 10:56:20 -0300

    image compression and saving

--- Sources/WDSign/WDSignDocumentView.swift
+++ Sources/WDSign/WDSignDocumentView.swift
@@ -29,8 +29,9 @@ public struct WDSign: View {
                         .frame(maxWidth: .infinity)
                     
                     Button(action: {
-                        let snapshot = documentView.snapshot()
-                        UIImageWriteToSavedPhotosAlbum(snapshot, nil, nil, nil)
+                        documentView
+                            .snapshot()
+                            .saveImageOnDocuments()
                     }, label: {
                         Text("Save")
                             .font(.headline)
@@ -119,6 +120,27 @@ extension View {
 
         return renderer.image { _ in
             view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+        }
+    }
+}
+
+extension UIImage {
+    func saveImageOnDocuments() {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        // choose a name for your image
+        let fileName = "image.jpg"
+        // create the destination file url to save your image
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        // get your UIImage jpeg data representation and check if the destination file url already exists
+        if let data = self.pngData(),
+          !FileManager.default.fileExists(atPath: fileURL.path) {
+            do {
+                // writes the image data to disk
+                try data.write(to: fileURL)
+                print("file saved")
+            } catch {
+                print("error saving file:", error)
+            }
         }
     }
 }

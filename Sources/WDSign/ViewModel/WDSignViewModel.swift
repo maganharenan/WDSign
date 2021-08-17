26f7e0fbf3f5d08d2960269949355e7f8e8709b6
Renan <maganha.renan@gmail.com>
2021-08-17 09:06:23 -0300

    last updates

--- Sources/WDSign/ViewModel/WDSignViewModel.swift
+++ Sources/WDSign/ViewModel/WDSignViewModel.swift
@@ -76,7 +76,7 @@ class WDSignViewModel: ObservableObject {
                               formRecordID: customerFormRecordID,
                               readOnly: 0)
         
-        SignLogDAO.instance.encodeSignatureLog(signLog: signLog) { success in
+        SignLogDAO.instance.persistSignatureLog(signLog: signLog) { success in
             completion(success)
         }
     }

0d1bd2f76f49dc8a85fa17bc7084149235b4f454
Renan <maganha.renan@gmail.com>
2021-10-25 09:16:10 -0300

    last updates

--- Sources/WDSign/View/SignatureBoxView.swift
+++ Sources/WDSign/View/SignatureBoxView.swift
@@ -68,7 +68,7 @@ struct SignatureBoxView: View {
             .frame(width: 540, height: 343, alignment: .center)
             .background(Color.white)
             .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-            .position(x: screen.width / 2, y: showModal ? (screen.height / 2) + dragGesturePosition : UIScreen.main.bounds.height * 2 + 200)
+            .position(y: showModal ? (screen.height / 2) + dragGesturePosition : UIScreen.main.bounds.height * 2 + 200)
             .animation(.easeInOut)
         }
     }
